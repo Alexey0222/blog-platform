24cb2c09169e497112c7373e3842453052c71c75
Alexey Terentyev <aleksej.terentev.97@mail.ru>
2025-03-04 23:58:44 +0300

    lasttoday

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Использует относительные пути
   build: {
     chunkSizeWarningLimit: 1000, // Увеличиваем лимит предупреждений
     cssCodeSplit: true, // Разделение CSS на отдельные файлы
